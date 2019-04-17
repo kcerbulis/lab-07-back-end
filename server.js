f1a4736107806c11a9376070c47f954a1fb02443
kcerbulis <kcerbulis@gmail.com>
2019-04-17 09:57:04 -0700

    initial files

--- server.js
+++ server.js
@@ -1,93 +1,70 @@
 'use strict';
 
-// immediate import and configuration
 require('dotenv').config();
-
-// global constants
-const PORT = process.env.PORT || 3000 ;
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const cors = require('cors');
 
-// server definition
 const app = express();
 app.use(cors());
 
-// what the server does
-//the route
-//request = data from query. example, from a front end query
-//can test in localhost:3000/location to verify
+function Geo_data_object(search_query, formatted_query, latitude, longitude) {
+  this.search_query = search_query;
+  this.formatted_query = formatted_query;
+  this.latitude = latitude;
+  this.longitude = longitude;
+}
+
+function Weather_data_object(forecast, time) {
+  this.forecast = forecast;
+  this.time = new Date(time);
+}
+
 app.get('/location', (request, response) => {
-  if(request.query.data !== 'lynnwood'){
-    response.status(500).send('yo, i only got the lynwood data')
+  response.send(search_location(request.query.data));
+})
+
+app.get('/weather', (request, response) => {
+  response.send(search_weather());
+})
+
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
+
+function search_location(front_end_query) {
+  const search_query = front_end_query;
+
+  const geo_data = require('./data/geo.json');
+  const formatted_query = geo_data.results[0].formatted_address;
+  const latitude = geo_data.results[0].geometry.location.lat;
+  const longitude = geo_data.results[0].geometry.location.lng;
+
+  const location_object = new Geo_data_object(search_query, formatted_query, latitude, longitude);
+
+  return location_object;
+}
+
+function search_weather() {
+  const weather_data = require('./data/darksky.json');
+  const weather_array = [];
+
+
+  for (let i = 0; i < 8; i++) {
+    const forecast = weather_data.daily.data[i].summary;
+    const time = weather_data.daily.data[i].time;
+    weather_array.push(new Weather_data_object(forecast, time));
   }
-  response.send( searchLatLng(request.query.data) );
-  // response.send({
-  //   'search_query': 'seattle',
-  //   'formatted_query': 'Seattle, WA, USA',
-  //   'latitude': '47.606210',
-  //   'longitude': '-122.332071'
-  // })
-})
 
-app.get('/weather', (request, response) =>{
-  const finalArrayOfWeather = [];
-  // get the data
-  const weatherJSON = require('./data/darksky.json');
+  // [{
+  //   'forecast': 'Partly cloudy until afternoon.',
+  //   'time': 'Mon Jan 01 2001'
+  // },
+  // {
+  //   'forecast': 'Mostly cloudy in the morning.',
+  //   'time': 'Tue Jan 02 2001'
+  // }
+  // ];
 
-  const dailyWeather = weatherJSON.daily;
-  const dailyWeatherData = dailyWeather.data;
-
-  dailyWeatherData.forEach(dayObj => {
-    finalArrayOfWeather.push(new DailyWeather(dayObj));
-  })
-
-  response.send(finalArrayOfWeather);
-})
-
-app.use('*', (request, response) => {
-  response.send('Our server runs.');
-})
-
-
-// ==============================================
-// Helper Functions
-// ==============================================
-
-function DailyWeather(rawDayObj){
-
-
-  this.forecast = rawDayObj.summary;
-  this.time = new Date (rawDayObj.time * 1000).toString().slice(0, 15);
+  return weather_array;
 }
 
-function searchLatLng(frontEndQuery) {
-  /*
-  'search_query': 'seattle',
-  //   'formatted_query': 'Seattle, WA, USA',
-  //   'latitude': '47.606210',
-  //   'longitude': '-122.332071'
-  */
-  // take the data from the front end, as the searched for location ('berlin')
-  const search_query = frontEndQuery;
-
-  // Go out and get data, tomorrow
-  const testData = require('./data/geo.json'); // go get some other data
-
-  const formatted_query = testData.results[0].formatted_address;
-  const results = testData.results;
-  const oneResult = results[0];
-  const geometry = oneResult.geometry;
-  const location = geometry.location;
-  const latitude = location.lat;
-  const longitude = location.lng;
-
-  const responseObject = { search_query, formatted_query, latitude, longitude };
-
-  return responseObject;
-
-}
-
-//server start
-app.listen(PORT, ()=> {
-  console.log(`app is up on PORT ${PORT}`)
-})
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
